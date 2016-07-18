998c1defa44a07f5a077f424f04ecf5f25d75428
codemattcode <codemattcode@gmail.com>
2016-07-18 21:42:18 +0100

    changelog

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
-'use strict';
+function showSize() {
+    var w = window.innerWidth;
+    // var h = window.innerHeight;
+    document.getElementById('size').innerHTML = "W: " + w ;
+}              // + " H: " + h; add if you also want height 
+showSize();
+
+// (function(){
+
+//   window.console.log('hello world!');
+
+// }());
 
